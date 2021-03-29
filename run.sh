31a0b5af6f1df0e4de6dd512b6cf617dff008bad
Dongwon Jang <dohan0930@gmail.com>
2021-03-30 01:52:21 +0900

    CHG: write run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/rocky112358/UNIST_CSE364_Group2.git
+cd UNIST_CSE364_Group2
+mvn install
+java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar se.group2.Movieblock Adventure educator

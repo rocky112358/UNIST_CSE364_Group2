74cf3344e849808ef49f57a4a11b860ad2e59db4
Dongwon Jang <dohan0930@gmail.com>
2021-05-01 19:26:01 +0900

    CHG: add echo newline character in run.sh for visibility

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@ git clone https://github.com/rocky112358/UNIST_CSE364_Group2.git
 cd UNIST_CSE364_Group2
 mvn install
 java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar se.group2.Movieblock "" "" ""
+echo -e "\n"
 java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar se.group2.Movieblock "" "" "" "Adventure"

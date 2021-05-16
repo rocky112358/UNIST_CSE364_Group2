78a29069045d69bc407296b095430edf7d632f51
Dongwon Jang <dohan0930@gmail.com>
2021-05-17 02:22:08 +0900

    CHG: Get imdb id from links.dat file and construct imdb url

--- src/main/java/se/group2/MovieblockController.java
+++ src/main/java/se/group2/MovieblockController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,6 +17,33 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class MovieblockController {
+    private static List<Link> links;
+
+    public static void loadLinks(String filename) {
+        List<Link> l = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] properties = line.split("::");
+                Link link = new Link(properties[0], properties[1]);
+                l.add(link);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        links = l;
+    }
+
+    public static String printMovie(Movie movie) {
+        for (Link l: links) {
+            if (l.movieId == movie.id) {
+                return String.format("http://www.imdb.com/title/tt%s", l.imdbId);
+            }
+        }
+        return null;
+    }
 
     @RequestMapping(value = "/users/recommendations", method = GET, consumes = {MediaType.APPLICATION_JSON_VALUE})
     public List<RecommendationOutput> userRecommendations(@RequestBody UserRecommendationInput input) {
@@ -30,10 +60,11 @@ public class MovieblockController {
         RecommendationEngine engine = new RecommendationEngine();
         List<Movie> recommendationResults = engine.recommendMovies(input.getGender(), input.getAge(), occupationNo, genresInput, "", 10);
 
-        // TODO: get imdb id from links.dat file and construct imdb url
+        loadLinks("data/links.dat");
+
         return recommendationResults.stream().map(
                 (movie -> new RecommendationOutput(
-                        movie.title, String.join("|", movie.genre), "https://naver.com"))
+                        movie.title, String.join("|", movie.genre), printMovie(movie)))
         ).collect(Collectors.toList());
     }
 
@@ -46,11 +77,11 @@ public class MovieblockController {
         List<String> genresInput = new ArrayList<>();
         List<Movie> recommendationResults = engine.recommendMovies("", "", -1, genresInput, input.getTitle(), input.getLimit());
 
-        // TODO: get imdb id from links.dat file and construct imdb url
+        loadLinks("data/links.dat");
+
         return recommendationResults.stream().map(
                 (movie -> new RecommendationOutput(
-                        movie.title, String.join("|", movie.genre), "https://naver.com"))
+                        movie.title, String.join("|", movie.genre), printMovie(movie)))
         ).collect(Collectors.toList());
     }
-
 }

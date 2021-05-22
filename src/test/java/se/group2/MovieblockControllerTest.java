1fa1f48f3114dbc63af8532dbefde303062c9c9f
Dongwon Jang <dohan0930@gmail.com>
2021-05-23 01:52:48 +0900

    CHG: add junit.Rule

--- src/test/java/se/group2/MovieblockControllerTest.java
+++ src/test/java/se/group2/MovieblockControllerTest.java
@@ -3,6 +3,8 @@ package se.group2;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 import java.util.*;
 

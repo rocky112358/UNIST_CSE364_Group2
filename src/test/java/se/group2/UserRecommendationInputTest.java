5e808dd1f87e2662a15f172637e79f92c781f3f0
Dongwon Jang <dohan0930@gmail.com>
2021-05-22 14:09:11 +0000

    ADD: blank test files

---
+++ src/test/java/se/group2/UserRecommendationInputTest.java
@@ -0,0 +1,12 @@
+package se.group2;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;

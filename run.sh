0d5a45752c8fa22e9437591539d2a9d8400d4718
Dongwon Jang <dohan0930@gmail.com>
2021-05-22 13:15:04 +0000

    CHG: Add Spring Boot Maven Plugin and change run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 git clone https://github.com/rocky112358/UNIST_CSE364_Group2.git
 cd UNIST_CSE364_Group2
-mvn install
+mvn package
 echo ""
-java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar se.group2.Movieblock "" "" ""
-echo ""
-java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar se.group2.Movieblock "" "" "" "Adventure"
+java -jar target/cse364-project-1.0-SNAPSHOT.jar

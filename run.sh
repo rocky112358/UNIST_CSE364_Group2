cba3046bbab8acc86c575d39b5e15fe3d9d21ac0
root <Dongwon Jang>
2021-05-24 18:41:35 +0000

    CHG: change run.sh for integration test

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ git clone https://github.com/rocky112358/UNIST_CSE364_Group2.git
 cd UNIST_CSE364_Group2
 mvn package
 echo ""
-java -jar target/cse364-project-1.0-SNAPSHOT.jar
+java -jar target/cse364-project-1.0-SNAPSHOT-exec.jar

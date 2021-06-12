193051ec0cf64da8e3c828df6f1d1c1a8a041dd7
Dongmin <rocky112358@gmail.com>
2021-06-12 16:08:49 +0900

    DEL: remove redundant lines

--- src/main/java/se/group2/LinkRepository.java
+++ src/main/java/se/group2/LinkRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface LinkRepository extends MongoRepository<Link, String> {
-    public List<Link> findAll();
     public Link findByMovieId(int id);
 }
--- src/main/java/se/group2/MovieRepository.java
+++ src/main/java/se/group2/MovieRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface MovieRepository extends MongoRepository<Movie, String> {
-    public List<Movie> findAll();
     public Movie findById(int id);
     public List<Movie> findByGenre(String genre);
     public List<Movie> findByTitle(String title);

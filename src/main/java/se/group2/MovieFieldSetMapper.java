2c9c70fb1816d3bb41f937d5c03b5543fece8a4a
Dongmin <rocky112358@gmail.com>
2021-06-12 16:06:31 +0900

    FIX: cast movie genres to lowercase before insert to DB

--- src/main/java/se/group2/MovieFieldSetMapper.java
+++ src/main/java/se/group2/MovieFieldSetMapper.java
@@ -4,10 +4,11 @@ import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.FieldSet;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class MovieFieldSetMapper implements FieldSetMapper {
     @Override
     public Movie mapFieldSet(FieldSet fieldSet) {
-        return new Movie(Integer.parseInt(fieldSet.readString(0)), fieldSet.readString(1), Arrays.asList(fieldSet.readString(2).split("\\|")));
+        return new Movie(Integer.parseInt(fieldSet.readString(0)), fieldSet.readString(1), Arrays.stream(fieldSet.readString(2).split("\\|")).map(String::toLowerCase).collect(Collectors.toList()));
     }
 }
